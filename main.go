2f221768c16379a5e8d606ec2bfeb76fcfd3ac12
Alistair Smith <alistairsmith01@gmail.com>
2021-12-09 04:39:37 +0000

    feat: hello world ping/pong

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
@@ -12,11 +14,21 @@ func main() {
 
 	session, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 
+	session.Identify.Intents = discordgo.IntentsGuildMessages
+
 	if err != nil {
 		panic(err)
 	}
 
 	session.AddHandler(messageCreate)
+
+	session.Open()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
+	<-sc
+
+	session.Close()
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
