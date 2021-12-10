7a259bd52164ba7261553507e355b42dfff4b5d6
cnrad <conradwcrawford@gmail.com>
2021-12-10 15:59:16 -0500

    clean up verification message

--- src/members.go
+++ src/members.go
@@ -5,5 +5,5 @@ import (
 )
 
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-	s.ChannelMessageSend(VERIFICATION_CHANNEL_ID, "Hey "+m.Mention()+" in order to get access to the community, you'll need to send an image with your wumpus plushie next to a note containg your discord tag in this channel.")
+	s.ChannelMessageSend(VERIFICATION_CHANNEL_ID, "Hey "+m.Mention()+", in order to get access to the community, please send an image of your Wumpus plushie next to a note containg your Discord username and tag in this channel.")
 }
