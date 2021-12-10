cc1e2d5a2a547a983e1af9811e292056680b9ed0
Dustin Rouillard <code@dstn.to>
2021-12-10 11:50:10 -0700

    Comments go poof

--- main.go
+++ main.go
@@ -13,30 +13,30 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
-// Create environment variables locally for base development
+// Channel variables
 var CDN_CHANNEL_ID = getenv("CDN_CHANNEL", "918725182330400788")
 var PICS_CHANNEL_ID = getenv("PICS_CHANNEL", "918355152493215764")
+var VERIFICATION_CHANNEL_ID = getenv("VERIFICATION_CHANNEL", "918932836428419163")
+
+// Role variables
 var TEAM_ROLE_ID = getenv("TEAM_ROLE", "918354701337116703")
 
 func main() {
 	// Create a new Discord session using the provided bot token.
 	session, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 
-	session.Identify.Intents = discordgo.IntentsGuildMessages
+	session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMembers
 
-	// If there is an error, print it and return.
 	if err != nil {
 		panic(err)
 	}
 
 	session.AddHandler(messageCreate)
-	// session.AddHandler(guildMemberAdd)
+	session.AddHandler(guildMemberAdd)
 
-	// Connect to the Discord API with the token provided -- Session.Open();
 	session.Open()
 
-	/* When bot starts up -- Log to the console */
-	fmt.Println("🚀 Bot has launched")
+	fmt.Println("🚀 Wumpus has launched :D")
 
 	session.UpdateGameStatus(0, "big wumpus")
 
@@ -44,48 +44,48 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
 	<-sc
 
-	/* When crash -> Print Error */
-	fmt.Println("🛑 Uh oh! It appears that an error has occured.")
+	fmt.Println("uh oh D:")
 
 	session.Close()
 }
 
-// func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-// 	/* idk man we could probably do something here */
-// }
+func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+	fmt.Println("Member joined", m.User.Username)
+}
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// If the user is the bot, return the function.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// This if statement prevents people from running commands in the pics channel
 	if m.ChannelID == PICS_CHANNEL_ID {
-		if len(m.Attachments) > 0 {
+		if len(m.Attachments) > 0 && m.Attachments[0].Height != 0 && m.Attachments[0].Width != 0 {
 			image := m.Attachments[0]
-
-			// Fetch attachment
 			request, _ := http.Get(image.URL)
 
-			// Send attachment in the CDN channel
 			cdnMessage, _ := s.ChannelFileSend(CDN_CHANNEL_ID, image.Filename, request.Body)
 
 			s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-
-				// If image is provided, embed it
 				Type:        "rich",
 				Color:       2617723,
 				Image:       &discordgo.MessageEmbedImage{URL: cdnMessage.Attachments[0].URL},
 				Author:      &discordgo.MessageEmbedAuthor{IconURL: m.Author.AvatarURL(""), Name: m.Author.Username},
 				Description: m.Content,
 			})
+
 			s.ChannelMessageDelete(m.ChannelID, m.ID)
 		} else {
-			// If it doesn't have an attachment of an image return an error
 			s.ChannelMessageDelete(m.ChannelID, m.ID)
 			message, _ := s.ChannelMessageSend(m.ChannelID, "Please include an image!")
+			time.AfterFunc(5*time.Second, func() { s.ChannelMessageDelete(m.ChannelID, message.ID) })
+		}
+	} else if m.ChannelID == VERIFICATION_CHANNEL_ID {
+		if len(m.Attachments) > 0 && m.Attachments[0].Height != 0 && m.Attachments[0].Width != 0 {
+			s.MessageReactionAdd(m.ChannelID, m.ID, "👍")
+			s.MessageReactionAdd(m.ChannelID, m.ID, "👎")
+		} else {
+			s.ChannelMessageDelete(m.ChannelID, m.ID)
+			message, _ := s.ChannelMessageSend(m.ChannelID, "Must include an image to get verified!")
 			time.AfterFunc(5*time.Second, func() { s.ChannelMessageDelete(m.ChannelID, message.ID) })
 		}
 	} else {
