3a847fdb11bb6450cf04fd1e966b224e7dc39963
Dustin Rouillard <code@dstn.to>
2021-12-10 13:29:47 -0700

    Removing another pointless log

--- src/members.go
+++ src/members.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
 )
 
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-	fmt.Println("Member joined", m.User.Username)
-
 	s.ChannelMessageSend(VERIFICATION_CHANNEL_ID, "Hey "+m.Mention()+" in order to get access to the community, you'll need to send an image with your wumpus plushie next to a note containg your discord tag in this channel.")
 }
