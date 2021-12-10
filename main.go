d5c0110d28897bb90ca252653c99809b18d3aaec
Dustin Rouillard <code@dstn.to>
2021-12-10 11:50:49 -0700

    Last ones

--- main.go
+++ main.go
@@ -13,16 +13,13 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
-// Channel variables
 var CDN_CHANNEL_ID = getenv("CDN_CHANNEL", "918725182330400788")
 var PICS_CHANNEL_ID = getenv("PICS_CHANNEL", "918355152493215764")
 var VERIFICATION_CHANNEL_ID = getenv("VERIFICATION_CHANNEL", "918932836428419163")
 
-// Role variables
 var TEAM_ROLE_ID = getenv("TEAM_ROLE", "918354701337116703")
 
 func main() {
-	// Create a new Discord session using the provided bot token.
 	session, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 
 	session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMembers
