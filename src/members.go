902f32385b212dea668e55ae18e07ec5f49c56cb
Dustin Rouillard <code@dstn.to>
2021-12-19 12:28:08 -0700

    Correct mispelling of "containing"

--- src/members.go
+++ src/members.go
@@ -7,7 +7,7 @@ import (
 )
 
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
-	s.ChannelMessageSend(utils.VERIFICATION_CHANNEL_ID, "Hey "+m.Mention()+", in order to get access to the community, please send an image of your Wumpus plushie next to a note containg your Discord username and tag in this channel.")
+	s.ChannelMessageSend(utils.VERIFICATION_CHANNEL_ID, "Hey "+m.Mention()+", in order to get access to the community, please send an image of your Wumpus plushie next to a note containing your Discord username and tag in this channel.")
 }
 
 func guildMemberUpdate(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
